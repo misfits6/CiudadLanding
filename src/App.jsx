236b2d3ef211a25b08e164ba5bbb13925c345930
jhon <kbtotal90@gmail.com>
2024-04-26 14:00:18 -0500

    add video loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Novatrix, Opulento, Tranquiluxe } from "uvcanvas";
+
+import videoBg from "./assets/Entropia.mp4";
 
 function App() {
   return (
-    <div className="flex justify-center items-center align-middle">
-      <img
-        className=" justify-center items-center align-middle"
-        src="entropia.jpeg"
-        alt=""
-      />
-    </div>
+    <>
+      <div className="flex justify-center items-center align-middle">
+        {/* <img
+          className=" justify-center items-center align-middle"
+          src="entropia.jpeg"
+          alt=""
+        /> */}
+        <video src={videoBg} autoPlay loop />
+      </div>
+
+      <div className="bg-inherit">
+        <Novatrix />
+      </div>
+    </>
   );
 }
 
